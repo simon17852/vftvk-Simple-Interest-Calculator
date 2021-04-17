42ed842652cc401332f0728126ea2ed98392184e
Simon Ip <ipheungming17852@gmail.com>
2021-04-17 16:26:51 +0800

    Second Commit

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ function compute()
       var interest = principal * years * rate / 100;
       var year = new Date().getFullYear() + parseInt(years);
 
-      document.getElementById("result").innerHTML="If you deposit " + principal+", \<br\>at an interest rate of " +
-          rate + "%\<br\>You will receive an amount of "+ interest +",\<br\>in the year "+year+"\<br\>"
+      document.getElementById("result").innerHTML="If you deposit \<mark\>" + principal + "\</mark\>, \<br\>at an interest rate of \<mark\>" +
+          rate + "\</mark\> %\<br\>You will receive an amount of \<mark\>"+ interest +"\</mark\>,\<br\>in the year \<mark\>"+year+"\</mark\> \<br\>"
    }
 }
 
